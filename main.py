de7c7af656a28b3e6ecf70530597f7bb8697c2b6
Quinten <90308786+QNoah@users.noreply.github.com>
2024-09-23 10:20:38 +0200

    menu added

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+import os
 from products_list import Products_List
 from product import Product
 
+def menu():
+    os.system("cls")
+    print("Menu:")
+    print("1. Add product")
+    print("2. Remove product")
+    print("3. View List")
+    choice = int(input(""))
+    try:
+        if choice == 1:
+            print("keuze 1")
+    except ValueError:
+        print("That is not a valid option.")
+        menu()
+
 product_list = Products_List()
 
-product_1 = Product(1, "Egg", 2)
-# qty = int(input())
-product_list.add_product(product_1)
+# product_1 = Product(1, "Egg", 2)
+# # qty = int(input())
+# product_list.add_product(product_1)
 
-print(product_list.show_list())
+# print(product_list.show_list())
 
-# print("Welcome before we continue to visit your entire list of products, we first need to enter some products.")
+print("Welcome before we continue to visit your entire list of products, we first need to enter some products.")
+product_name = input("Enter a name for your product: ")
+product_qty = int(input("Enter the amount of quantity: "))
+
+menu()
